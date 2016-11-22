fa8c9ba1fbdca4c4a32f837f8f963b2b8ee73f1f
thai quang vinh <ghiteze@gmail.com>
2016-11-22 12:04:45 +0700

    ed

--- src/android/Hello.java
+++ src/android/Hello.java
@@ -2,31 +2,31 @@ package com.example.plugins;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
+import org.apache.cordova.PluginResult;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import android.provider.Settings;
+import android.content.Intent;
 
 
 public class Hello extends CordovaPlugin {
 
   @Override
   public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-
+    PluginResult.Status status = PluginResult.Status.OK;
     if (action.equals("hi")) {
 
       String name = args.getString(0);
 
-      if (name != null && name.length() > 0) {
-        callbackContext.success("Hello " + name);
-      }
-      else {
-        callbackContext.error("Error");
-      }
+      Intent intent = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+      intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+      callbackContext.sendPluginResult(new PluginResult(status, ""));
 
       return true;
     }
     return false;
-
   }
 }
